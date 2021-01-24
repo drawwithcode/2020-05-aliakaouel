0f51bbbf09175b1443e11eff4351a87162741a6f
aliakaouel <31700227+aliakaouel@users.noreply.github.com>
2021-01-25 00:57:45 +0100

    added image

--- public/sketch.js
+++ public/sketch.js
@@ -1,24 +1,26 @@
 let socket = io();
-let myColor = "white";
+let myColor = "pink";
+let myworld;
 
 socket.on("connect", newConnection);
 socket.on("mouseBroadcast", drawOtherMouse);
 socket.on("color", setColor);
 socket.on("newPlayer", newPlayer);
 
+//text newPlayer
 function newPlayer(newPlayerColor){
   console.log(newPlayerColor);
 
-  push();
-  fill("lightblue");
-  noStroke();
-  rectMode(CENTER);
-  rect(width / 2, height / 2, windowWidth, 50);
-
-  textSize(30)
-  fill(newPlayerColor);
-  text("New player joined: " + newPlayerColor, width / 2, height / 2);
-  pop();
+  // push();
+  // fill("lightblue");
+  // noStroke();
+  // rectMode(CENTER);
+  // rect(width / 2, height / 2, windowWidth, 50);
+  //
+  // textSize(30)
+  // fill(newPlayerColor);
+  // text("New player joined: " + newPlayerColor, width / 2, height / 2);
+  // pop();
 }
 
 function setColor(assignedColor){
@@ -31,6 +33,7 @@ function newConnection(){
 
 function drawOtherMouse(data){
  push();
+ noStroke();
  fill(data.color);
   ellipse(data.x, data.y, 10);
  pop();
@@ -38,28 +41,37 @@ function drawOtherMouse(data){
 
 function preload(){
   // put preload code here
+  myworld = loadImage('myworld.png');
+
+
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
   // put setup code here
-  background("lightblue");
+  background("white");
    //welconme
-   push()
-   textSize(30);
-   textAlign("center");
-   fill(myColor);
-   text("welcome" + myColor, width / 2, height / 2);
-   pop();
+   // push()
+   // textSize(30);
+   // textAlign("center");
+   // fill(myColor);
+   // text("welcome" + myColor, width / 2, height / 2);
+   // pop();
+
+  imageMode(CENTER);
+   image(myworld, width/2, height/2, width,height);
+   text('Color the Wold, create a dream together!')
 }
 
 function draw() {
   // put drawing code here
+
 }
 
-function mouseMoved(){
+function mouseDragged(){
   push();
-  fill("myColor");
+  noStroke();
+  fill("pink");
   ellipse(mouseX, mouseY, 10);
   pop();
 
